413e000ef0a37d5ed6a04d4de94e101d37f99ffc
Tomar <2414293@cognizant.com>
2025-06-09 16:15:35 +0530

    Cobra's commit

--- src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
@@ -9,6 +9,9 @@ public class InventoryManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemApplication.class, args);
 		System.out.println("hello");
+		System.out.println("Tejas KingCobra");
+
+		
 		
 	}
 
