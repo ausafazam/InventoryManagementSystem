3601af805ec8ed82a9255236da920618e239462a
Saxena <2414390@cognizant.com>
2025-06-09 15:33:47 +0530

    my local branch commit

--- src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
@@ -9,10 +9,6 @@ public class InventoryManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemApplication.class, args);
 		System.out.println("hello");
-		System.out.println("Tejas KingCobra");
-		System.out.println("Anant saxena Ananconda");
-		System.out.println("Ghouse");
-		System.out.println("Ghouse hi");
 		
 	}
 
