baa5c5a52b1729d9847e524addf2eab2f53b0bb7
Azam <2414304@cognizant.com>
2025-06-09 14:45:48 +0530

    packages creation commit

--- src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class InventoryManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
