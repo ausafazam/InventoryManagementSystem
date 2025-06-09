22ba8520671bdce79a36dbf8a0559ee9579f4d28
Tomar <2414293@cognizant.com>
2025-06-09 15:21:18 +0530

    Ghouse updated to kechli

--- src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/inventory/InventoryManagementSystem/InventoryManagementSystemApplication.java
@@ -12,7 +12,7 @@ public class InventoryManagementSystemApplication {
 		System.out.println("Tejas KingCobra");
 		System.out.println("Anant Ananconda");
 		System.out.println("Ghouse");
-		System.out.println("Ghouse");
+		System.out.println("Ghouse kechli");
 		
 		
 	}
