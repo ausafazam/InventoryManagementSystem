0524275503f17306fd1c566e412d833c1cdaba24
S <2414375@cognizant.com>
2025-06-10 11:29:24 +0530

    dto code added

---
+++ src/main/java/com/inventory/InventoryManagementSystem/dto/Response.java
@@ -0,0 +1,51 @@
+package com.inventory.InventoryManagementSystem.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.inventory.InventoryManagementSystem.enums.UserRole;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class Response {
+	//generic 
+	private int status;
+	private String message;
+	//for login
+	private String token;
+	private UserRole role;
+	private String expirationTime;
+	
+	
+	//for pagination
+	private Integer totalPages;
+	private Long totalElement;
+	
+	
+	//data output optional
+	private UserDTO user;
+	private List<UserDTO> users;
+	
+	private SupplierDTO supplier;
+	private List<SupplierDTO> suppliers;
+	
+	private CategoryDTO category;
+	private List<CategoryDTO> categories;
+	
+	private ProductDTO product;
+	private List<ProductDTO> products;
+	
+	
+	private TransactionDTO transaction;
+	private List<TransactionDTO> transactions;
+	
+	private final LocalDateTime timestamp = LocalDateTime.now();
+	
+
+}
---
+++ src/main/java/com/inventory/InventoryManagementSystem/dto/TransactionDTO.java
@@ -0,0 +1,56 @@
+package com.inventory.InventoryManagementSystem.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.inventory.InventoryManagementSystem.enums.TransactionStatus;
+import com.inventory.InventoryManagementSystem.enums.TransactionType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionDTO {
+
+    private Long id;
+
+    private Integer totalProducts;
+
+    private BigDecimal totalPrice;
+
+    private TransactionType transactionType;
+
+    private TransactionStatus status;
+
+    private String description;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime createdAt;
+
+    private UserDTO user;
+
+    private ProductDTO product;
+
+    private SupplierDTO supplier;
+
+
+}
