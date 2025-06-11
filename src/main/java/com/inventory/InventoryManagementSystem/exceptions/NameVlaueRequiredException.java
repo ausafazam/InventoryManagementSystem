bc5d782d212ed5239ede915782fb11e7c4f3b349
Tomar <2414293@cognizant.com>
2025-06-11 10:19:35 +0530

    Exception code Added

---
+++ src/main/java/com/inventory/InventoryManagementSystem/exceptions/NameVlaueRequiredException.java
@@ -0,0 +1,9 @@
+package com.inventory.InventoryManagementSystem.exceptions;
+
+public class NameVlaueRequiredException extends RuntimeException{
+
+    public NameVlaueRequiredException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/inventory/InventoryManagementSystem/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.inventory.InventoryManagementSystem.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message){
+        super(message);
+    }
+
+}
