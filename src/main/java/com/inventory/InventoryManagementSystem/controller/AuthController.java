fe6f158231bce6dd6d83b1957c0805c6c2421411
Saxena <2414390@cognizant.com>
2025-06-17 20:54:42 +0530

    anant commit

--- src/main/java/com/inventory/InventoryManagementSystem/controller/AuthController.java
+++ src/main/java/com/inventory/InventoryManagementSystem/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.inventory.InventoryManagementSystem.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class AuthController {
 	
 	private final UserService userService;
 	
+	@PostMapping("/register")
 	public ResponseEntity<Response> registerUser(@RequestBody @Valid RegisterRequest registerRequest){
 		return ResponseEntity.ok(userService.registerUser(registerRequest));
 	}
