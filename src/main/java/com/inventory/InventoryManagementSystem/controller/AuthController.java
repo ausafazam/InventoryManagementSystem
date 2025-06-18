371ebda6ec91bed6e43491adb74df81d090d0edf
S <2414375@cognizant.com>
2025-06-18 10:10:55 +0530

    user commit

--- src/main/java/com/inventory/InventoryManagementSystem/controller/AuthController.java
+++ src/main/java/com/inventory/InventoryManagementSystem/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.inventory.InventoryManagementSystem.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.inventory.InventoryManagementSystem.dto.LoginRequest;
 import com.inventory.InventoryManagementSystem.dto.RegisterRequest;
 import com.inventory.InventoryManagementSystem.dto.Response;
 import com.inventory.InventoryManagementSystem.service.UserService;
@@ -19,8 +21,15 @@ public class AuthController {
 	
 	private final UserService userService;
 	
+	@PostMapping("/register")
 	public ResponseEntity<Response> registerUser(@RequestBody @Valid RegisterRequest registerRequest){
 		return ResponseEntity.ok(userService.registerUser(registerRequest));
 	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<Response> loginUser(@RequestBody @Valid LoginRequest loginRequest){
+		return ResponseEntity.ok(userService.loginUser(loginRequest));
+	}
+
 
 }
--- src/main/java/com/inventory/InventoryManagementSystem/service/UserService.java
+++ src/main/java/com/inventory/InventoryManagementSystem/service/UserService.java
@@ -1,10 +1,25 @@
 package com.inventory.InventoryManagementSystem.service;
 
+import com.inventory.InventoryManagementSystem.dto.LoginRequest;
 import com.inventory.InventoryManagementSystem.dto.RegisterRequest;
 import com.inventory.InventoryManagementSystem.dto.Response;
+import com.inventory.InventoryManagementSystem.dto.UserDTO;
+import com.inventory.InventoryManagementSystem.entity.User;
 
 public interface UserService {
 	
 	Response registerUser(RegisterRequest registerRequest);
+	Response loginUser(LoginRequest loginRequest);
+	
+	Response getAllUsers();
+	
+	User getCurrentLoggedInUser();
+	
+	Response updateUser(Long id, UserDTO userDTO);
+	
+	Response deleteUser(Long id);
+	
+	Response grtUsersTransaction(Long id);
+	
 
 }
